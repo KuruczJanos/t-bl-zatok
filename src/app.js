23fd0bc2919af66e6b4efa079eb72ed630ea8e94
Kurucz <kuruczjanoshivatalos@gmail.com>
2023-02-07 17:19:33 +0100

    Óra eleje

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Kurucz János
+* Copyright: 2023, Kurucz János
+* Group: Szoft 1/I/E
+* Date: 2023-02-04
+* Github: https://github.com/KuruczJanos/t-bl-zatok.git
+* Licenc: GNU GPL
+*/
